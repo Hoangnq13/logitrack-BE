54b6a388e2d03a3bf2c22bc2d683eb9b5277b278
hoangnq <hoang.nq.13@gmail.com>
2026-02-28 14:35:05 +0000

    config CORS

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,8 +4,12 @@ import { ApiGatewayModule } from './api-gateway.module';
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
-  // Cho phép Frontend ở cổng HTTP khác (như 8080) gọi được vào API Gateway (cổng 3000)
-  app.enableCors();
+  app.enableCors({
+    // Cho phép cổng 8080 (HTML test cũ) và 3001 (Next.js FE mới)
+    origin: ['http://localhost:8080', 'http://localhost:3001', 'http://127.0.0.1:8080', 'http://127.0.0.1:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   await app.listen(process.env.port ?? 3000);
 }
